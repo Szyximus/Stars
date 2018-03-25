e3ba87a419a0327ffe8d871e7049154247079675
Szymon Jakóbczyk <sjakobczyk@gmail.com>
2018-03-25 18:12:02 +0200

    Minimap stage 1 done

--- Assets/Scripts/UI/MiniMapController.cs
+++ Assets/Scripts/UI/MiniMapController.cs
@@ -8,12 +8,12 @@ public class MiniMapController : MonoBehaviour
 
     //Singleton
     public static MiniMapController main;
-    public CameraController controller;
+    public CameraController controller; //Main camera Controller
 
 
     public Camera MainCamera;
     public Camera MiniMapCamera;
-    public Material LineMat;
+    public Material LineMat; //Material of the camera trapezoid
 
     Vector2 lastClicked;
 
@@ -23,6 +23,11 @@ public class MiniMapController : MonoBehaviour
     Vector3 b;
     Vector3 c;
     Vector3 d;
+
+    RectTransform rectTransform; //Transform data of the minimap, it scales with the resolution
+
+
+
 
     void Awake()
     {
@@ -40,16 +45,21 @@ public class MiniMapController : MonoBehaviour
         // Set the number of vertex fo the Line Renderer
         line.positionCount = 5;
         line.material = LineMat;
-        line.gameObject.layer = 8;
+        line.gameObject.layer = 8; // Set trapezoid layer to 8=minimap, so it doesn't show to the main camera
+        
+
+
     }
 
     void Update()
     {
-        if (Input.GetButton("MouseLeft") && Input.mousePosition.x > 8 && Input.mousePosition.x < 256 + 8 && Input.mousePosition.y > 8 && Input.mousePosition.y < 192 + 8 )
+        RectTransform rectTransform = gameObject.GetComponent<RectTransform>();
+
+        if (Input.GetButton("MouseLeft") && Input.mousePosition.x > 8 && Input.mousePosition.x < rectTransform.rect.width + 8 && Input.mousePosition.y > 8 && Input.mousePosition.y < rectTransform.rect.height + 8 )
         {
             controller.LookAt(lastClicked);
         }
-        //Bottom left
+        //Bottom left corner
         Ray ray1 = MainCamera.ScreenPointToRay(new Vector3(1, 1, 0));
 
         //Bottom right
@@ -63,16 +73,16 @@ public class MiniMapController : MonoBehaviour
 
         //Find world co-ordinates
         RaycastHit hit;
-        Physics.Raycast(ray1, out hit, 500, (1 << 1));
+        Physics.Raycast(ray1, out hit, 500, (1 << 9));
         Vector3 v1 = hit.point;
 
-        Physics.Raycast(ray2, out hit, 500, (1 << 1));
+        Physics.Raycast(ray2, out hit, 500, (1 << 9));
         Vector3 v2 = hit.point;
 
-        Physics.Raycast(ray3, out hit, 500, (1 << 1));
+        Physics.Raycast(ray3, out hit, 500, (1 << 9));
         Vector3 v3 = hit.point;
 
-        Physics.Raycast(ray4, out hit, 500, (1 << 1));
+        Physics.Raycast(ray4, out hit, 500, (1 << 9));
         Vector3 v4 = hit.point;
 
         a.Set(50, 0, 60);
@@ -84,21 +94,23 @@ public class MiniMapController : MonoBehaviour
         line.SetPosition(1, v2);
         line.SetPosition(2, v3);
         line.SetPosition(3, v4);
-        line.SetPosition(4, v1);
+        line.SetPosition(4, v1); //draw trapezoid
+        
     }
 
     public void OnPointerDown(PointerEventData eventData)
     {
+        RectTransform rectTransform = gameObject.GetComponent<RectTransform>();
 
         Vector2 Target;
         Target.x = eventData.position.x - 8;
         Target.y = eventData.position.y - 8;
 
-        Target.x = MiniMapCamera.transform.position.x + (Target.x - 256 / 2f)/96 * MiniMapCamera.orthographicSize;
-        Target.y = MiniMapCamera.transform.position.z + (Target.y - 192 / 2f)/96 * MiniMapCamera.orthographicSize;
+        // calculate camera position from minimap click:
+        Target.x = MiniMapCamera.transform.position.x + (Target.x - rectTransform.rect.width / 2f)/ (rectTransform.rect.height / 2) * MiniMapCamera.orthographicSize;
+        Target.y = MiniMapCamera.transform.position.z + (Target.y - rectTransform.rect.height / 2f)/(rectTransform.rect.height/2) * MiniMapCamera.orthographicSize;
         lastClicked = Target;
 
 
-        Debug.Log("touched at " + Target);
     }
 }
