e56185ef62b9863cae1f5cecd34d96dd6085d315
Szymon Jakóbczyk <sjakobczyk@gmail.com>
2018-06-02 15:41:49 +0200

    Main Menu tweaks

---
+++ Assets/Scripts/UI/ClickableText.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Assets.Scripts;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class ClickableText : MonoBehaviour 
+    , IPointerEnterHandler
+    , IPointerExitHandler
+    , IPointerUpHandler
+{
+    private AudioSource sound;
+    private Shadow shadow;
+    // Use this for initialization
+    void Start () {
+        sound = gameObject.GetComponent<AudioSource>();
+        shadow = gameObject.GetComponent<Shadow>();
+
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        GetComponent<Transform>().localScale = new Vector3(1.05f, 1.05f, 1.05f);
+        shadow.effectDistance = new Vector2(0,0);
+   
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        GetComponent<Transform>().localScale = new Vector3(1, 1, 1);
+        shadow.effectDistance = new Vector2(3, -3);
+
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        sound.Play();
+    }
+}
