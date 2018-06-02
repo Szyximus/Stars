1dc7e42d4c3354435f831be370e89aa0328853a4
Szymon Jakóbczyk <sjakobczyk@gmail.com>
2018-06-02 23:33:46 +0200

    Simple log added

---
+++ Assets/Scripts/UI/ConsoleCanvas.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Assets.Scripts;
+using UnityEngine.UI;
+public class ConsoleCanvas : MonoBehaviour
+{
+
+    bool Shown;
+
+    private static bool created = false;
+    private ConsolePanel panel;
+
+    private void Awake()
+    {
+        if (!created)
+        {
+            //gameApp = GameObject.Find("GameApp").GetComponent<GameApp>();
+            panel = GetComponentInChildren<ConsolePanel>();
+            DontDestroyOnLoad(this.gameObject);
+            created = true;
+        }
+    }
+
+    void Start()
+    {
+        panel.enabled = false;
+        panel.gameObject.SetActive(false);
+        Shown = true;
+    }
+
+    void Show()
+    {
+        panel.enabled = true;
+        panel.gameObject.SetActive(true);
+        Shown = true;
+    }
+
+    void Hide()
+    {
+        panel.enabled = false;
+        panel.gameObject.SetActive(false);
+        Shown = false;
+    }
+
+    public void Toggle()
+    {
+        if (Shown) Hide();
+        else Show();
+    }
+
+    void Update()
+    {
+
+        if (Input.GetButtonUp("Console"))
+        {
+            Toggle();
+        }
+    }
+}
---
+++ Assets/Scripts/UI/ConsolePanel.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Assets.Scripts;
+using UnityEngine.UI;
+public class ConsolePanel : MonoBehaviour
+{
+    //#if !UNITY_EDITOR
+    static string myLog = "";
+    private string output;
+    private string stack;
+
+
+    void OnEnable()
+    {
+        Application.logMessageReceived += Log;
+    }
+
+    void OnDisable()
+    {
+        Application.logMessageReceived -= Log;
+    }
+
+    public void Log(string logString, string stackTrace, LogType type)
+    {
+        output = logString;
+        stack = stackTrace;
+        myLog = output + "\n" + myLog;
+        if (myLog.Length > 5000)
+        {
+            myLog = myLog.Substring(0, 4000);
+        }
+    }
+
+    void OnGUI()
+    {
+        {
+            myLog = GUI.TextArea(new Rect(25, 25, Screen.width - 25, Screen.height - 25), myLog);
+        }
+    }
+}
