526894b18a6e1d7c04fe9aa6aceb2d686f87c620
MinhNhat007 <minhnhattrinh1995@gmail.com>
2018-06-09 03:05:05 +0200

    update test Colonizing

--- Assets/Resources/Tests/TestScriptGamePlay.cs
+++ Assets/Resources/Tests/TestScriptGamePlay.cs
@@ -253,4 +253,107 @@ public class TestScriptGamePlay : HexGrid
         yield return new WaitForSeconds(3);
         yield return null;
     }
+
+    [UnityTest]
+    public IEnumerator TestColonizing()
+    {
+        SceneManager.LoadScene("MainMenuScene");
+        yield return new WaitForSeconds(1);
+        if (!SceneManager.GetActiveScene().name.Equals("MainMenuScene"))
+            Assert.Fail();
+
+        MainMenuSceneInit initializer = GameObject.Find("Initializer").GetComponent<MainMenuSceneInit>();
+
+        if (initializer == null)
+            Assert.Fail();
+
+        initializer.ChangeScene("NewGameMapScene");
+        yield return new WaitForSeconds(1);
+        if (!SceneManager.GetActiveScene().name.Equals("NewGameMapScene"))
+            Assert.Fail();
+
+
+        GameApp gameApp = GameObject.Find("GameApp").GetComponent<GameApp>();
+        if (gameApp == null)
+            Assert.Fail();
+        gameApp.PersistAllParameters(SceneManager.GetActiveScene().name);
+        gameApp.Parameters.Remove("ServerAddress");
+        gameApp.Parameters.Remove("ServerPort");
+        gameApp.Parameters.Add("ServerAddress", "127.0.0.1");
+        gameApp.Parameters.Add("ServerPort", "9999");
+
+        LevelLoader levelLoader = GameObject.Find("LevelLoader").GetComponent<LevelLoader>();
+        if (levelLoader == null)
+            Assert.Fail();
+        levelLoader.LoadLevel("NewGameScene");
+        yield return new WaitForSeconds(1);
+        if (!SceneManager.GetActiveScene().name.Equals("NewGameScene"))
+            Assert.Fail();
+
+        NewGameSceneInit newGameSceneInit = GameObject.Find("Initializer").GetComponent<NewGameSceneInit>();
+        if (newGameSceneInit == null)
+            Assert.Fail();
+        newGameSceneInit.AddPlayer();
+        newGameSceneInit.AddPlayer();
+
+        List<GameApp.PlayerMenu> playerMenuList = new List<GameApp.PlayerMenu>();
+        GameApp.PlayerMenu player1 = new GameApp.PlayerMenu
+        {
+            name = "player1",
+            password = "pass1",
+            race = "human",
+            playerType = "L"
+        };
+        GameApp.PlayerMenu player2 = new GameApp.PlayerMenu
+        {
+            name = "player12",
+            password = "pass2",
+            race = "human",
+            playerType = "L"
+        };
+        playerMenuList.Add(player1);
+        playerMenuList.Add(player2);
+        gameApp.SavePlayersFromMenu(playerMenuList);
+        ServerNetworkManager serverNetworkManager = GameObject.Find("ServerNetworkManager").GetComponent<ServerNetworkManager>();
+        serverNetworkManager.SetupNewGame();
+        yield return new WaitForSeconds(1);
+
+        GameController gameController = GameObject.Find("GameController").GetComponent<GameController>();
+        if (gameController == null)
+            Assert.Fail();
+
+        GameObject gameObjectKorriban = GameObject.Find("Korriban");
+        if (gameObjectKorriban == null)
+            Assert.Fail();
+
+        GameObject gameObjectspaceShip = Resources.Load("Object/Colonizer", typeof(GameObject)) as GameObject;
+        if (gameObjectspaceShip == null)
+            Assert.Fail();
+
+        Planet Korriban = gameObjectKorriban.GetComponent<Planet>();
+        gameController.GetCurrentPlayer().minerals = 100;
+        gameController.GetCurrentPlayer().population = 100;
+        gameController.GetCurrentPlayer().solarPower = 100;
+        EventManager.selectionManager.SelectedObject = gameObjectKorriban;
+        yield return new WaitForSeconds(2);
+        gameController.BuildSpaceship(gameObjectspaceShip);
+
+        GameObject gameObjectcolonizerSpaceShip = GameObject.Find("Colonizer(Clone)");
+        Colonizer colonizerSpaceShip = gameObjectcolonizerSpaceShip.GetComponent<Colonizer>();
+        if (colonizerSpaceShip == null)
+            Assert.Fail();
+        colonizerSpaceShip.actionPoints = 100;
+        GameObject gameObjetMoon = GameObject.Find("Moon");
+        Planet moon = gameObjetMoon.GetComponent<Planet>();
+        if (moon == null)
+            Assert.Fail();
+        colonizerSpaceShip.GetOwner().terraforming = 100;
+        EventManager.selectionManager.SelectedObject = gameObjectcolonizerSpaceShip;
+        yield return new WaitForSeconds(2);
+        EventManager.selectionManager.TargetObject = gameObjetMoon;
+        yield return new WaitForSeconds(2);
+        gameController.Colonize();
+         yield return new WaitForSeconds(2);
+        yield return null;
+    }
 }
